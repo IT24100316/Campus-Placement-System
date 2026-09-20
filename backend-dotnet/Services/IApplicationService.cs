e358b3f84b928972ea339cecd0fa82fab0a5c114
Pasindu Lakshan <IT24100316@my.sliit.lk>
2026-09-21 01:42:03 +0530

    Add DTOs for .NET Backend-CRUD

--- backend-dotnet/Services/IApplicationService.cs
+++ backend-dotnet/Services/IApplicationService.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using backend_dotnet.Models;
+using backend_dotnet.DTOs;
 
 namespace backend_dotnet.Services;
 
 public interface IApplicationService
 {
-    Task<IEnumerable<Application>> GetApplicationsByJobIdAsync(Guid jobId, int page, string status);
-    Task<IEnumerable<Application>> SearchApplicationsAsync(string query);
-    Task<Application> UpdateApplicationStatusAsync(Guid appId, string newStatus);
-    Task<Application> ScheduleInterviewAsync(Guid appId, DateTime date, TimeSpan time);
+    Task<IEnumerable<ApplicationResponseDto>> GetApplicationsByJobIdAsync(Guid jobId, int page, string status);
+    Task<IEnumerable<ApplicationResponseDto>> SearchApplicationsAsync(string query);
+    Task<Application> UpdateApplicationStatusAsync(Guid appId, UpdateStatusRequestDto request);
+    Task<Application> ScheduleInterviewAsync(Guid appId, ScheduleInterviewRequestDto request);
     Task<string> GetCvDownloadUrlAsync(Guid appId);
 }
