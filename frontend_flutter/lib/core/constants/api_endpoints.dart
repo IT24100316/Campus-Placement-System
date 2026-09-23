426a992c2a77349d54bb7ef4417c970078452b7b
IT24101748 <it24101748@my.sliit.lk>
2026-09-23 16:10:40 +0530

    centralize Flutter API endpoint configuration
    refactor(config): centralize Flutter API endpoint configuration

--- frontend_flutter/lib/core/constants/api_endpoints.dart
+++ frontend_flutter/lib/core/constants/api_endpoints.dart
@@ -1,19 +1,21 @@
+import '../config/api_config.dart';
+
 class ApiEndpoints {
-  static const String baseUrl = 'http://localhost:5168/api';
+  static String get baseUrl => ApiConfig.baseUrl;
 
   // Auth
-  static const String login = '$baseUrl/auth/login';
-  static const String registerStudent = '$baseUrl/auth/register-student';
+  static String get login => '$baseUrl/auth/login';
+  static String get registerStudent => '$baseUrl/auth/register-student';
 
   // Profile
-  static const String studentProfile = '$baseUrl/students/profile';
-  static const String uploadCv = '$baseUrl/students/upload-cv';
+  static String get studentProfile => '$baseUrl/students/profile';
+  static String get uploadCv => '$baseUrl/students/upload-cv';
 
   // Jobs
-  static const String jobsFeed = '$baseUrl/jobs';
+  static String get jobsFeed => '$baseUrl/jobs';
   static String jobDetails(String id) => '$baseUrl/jobs/$id';
 
   // Applications
-  static const String myApplications = '$baseUrl/applications/my-applications';
-  static const String applyJob = '$baseUrl/applications/apply';
+  static String get myApplications => '$baseUrl/applications/my-applications';
+  static String get applyJob => '$baseUrl/applications/apply';
 }
